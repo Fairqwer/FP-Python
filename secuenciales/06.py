4702209bb6d6c7e412ad74e3ef8da9e3695da6b1
Josue <josue.lc.2003@gmail.com>
2023-08-10 17:17:48 -0500

    ejemplo6

---
+++ secuenciales/06.py
@@ -0,0 +1,7 @@
+r=int(input("Radio del circulo: "))
+h=int(input("Altura del circulo: "))
+
+area = 2*3.1416*r*(r+h)
+volumen = 3.1416*(r*r)*h
+
+print(f"El área del circulo es {area} y el volumen es {volumen}")
