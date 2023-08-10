7610932f63803b816daa0196218d01e922cf4c00
Josue <josue.lc.2003@gmail.com>
2023-08-10 15:20:33 -0500

    ejemplo1

--- secuenciales/01.py
+++ secuenciales/01.py
@@ -1,1 +1,9 @@
-print("hola mundo")
+varones = int(input("varones : "))
+mujeres = int(input("mujeres : "))
+
+total= varones+mujeres
+
+pVarones = varones*100.0/total
+pMujeres = mujeres*100.0/total
+
+print( f"El porcentaje de varones es { format(pVarones, '.2f')}% y el porcentaje de mujeres es { format(pMujeres, '.2f')}%")
