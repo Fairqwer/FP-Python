f5ce26032c7edf795eec068b12bdb2468616a17f
Josue <josue.lc.26@gmail.com>
2023-08-14 16:26:26 -0500

    ejemplo7

---
+++ secuenciales/07.py
@@ -0,0 +1,8 @@
+b = int(input("Base del rectángulo: "))
+h = int(input("Altura del rectángulo: "))
+
+area = b*h
+perimetro = 2*(b+h)
+
+print(f"El área del rectángulo es {format (area, '.2f')} m y el perímetro es de {format (perimetro, '.2f')} m")
+
