0315911cfba72ae3fa6c462c7f595192e482dd45
Josue <josue.lc.2003@gmail.com>
2023-08-17 16:17:51 -0500

    condicionales30

--- condicionales/30.py
+++ condicionales/30.py
@@ -4,9 +4,9 @@ os.system("cls")
 cuota = int(input("Cuota : "))
 dia = int(input("Dìa de pago: "))
 
-if dia > 0 and dia <= 10 : descuento = 5 if 0.02*cuota < 5 else 0.02*cuota if 5 < 0.02*cuota else 0
-elif dia > 10 and dia <=20 : descuento = 0
+if dia <= 10 : descuento = 5 if 0.02*cuota < 5 else 0.02*cuota if 5 < 0.02*cuota else 0
 elif dia > 20 : descuento = -10 if -0.03*cuota < -10 else -0.03*cuota if -10 < -0.03*cuota else 0
+else : descuento = 0
 
 total = cuota - descuento
 
