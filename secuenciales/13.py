46da4142e9c2375753e8e1dbb760895ceb3f29c8
Josue <josue.lc.26@gmail.com>
2023-08-20 11:38:06 -0500

    secuenciales

--- secuenciales/13.py
+++ secuenciales/13.py
@@ -4,6 +4,6 @@ os.system("cls")
 ca = int(input("Cateto adyacente: "))
 co = int(input("Cateto opuesto: "))
 
-h = ((ca*ca) + (co*co))**0.5
+h = ((ca*ca) + (co*co)) ** 0.5
 
 print(f"La hipotenusa es {h:.2f}")
